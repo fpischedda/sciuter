1c489aae8ee27c9de299ec7b27444f2670c909c2
Francesco Pischedda <francesco.pischedda@redpineapplemedia.com>
2019-12-25 13:45:23 +0100

    added missing includes

---
+++ include/sciuter/animation.hpp
@@ -0,0 +1,32 @@
+#ifndef __SCIUTER__ANIMATION_HPP__
+#define __SCIUTER__ANIMATION_HPP__
+
+#include <map>
+#include <string>
+#include <vector>
+#include <sciuter/sdl.hpp>
+
+class Animation
+{
+    private:
+        std::vector<SDL_Rect> m_frames;
+        std::string m_name;
+
+    public:
+        Animation() {}
+        Animation(const std::vector<SDL_Rect> &frames, const std::string name="")
+            : m_frames(frames), m_name(name){}
+
+        const std::vector<SDL_Rect>& get_frames() { return m_frames; } 
+};
+
+typedef std::map<std::string, Animation> AnimationMap;
+
+class TexturePackerAnimationLoader
+{
+    public:
+        static AnimationMap load(const std::string filename);
+        static AnimationMap load(std::istream &input);
+};
+
+#endif
